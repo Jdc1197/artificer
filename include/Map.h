adcfef88956442d6d0814bf5eca21cf48b708bf1
Jdc1197 <Jdc1197@gmail.com>
2013-06-04 21:12:21 -0500

    Added documentation for doxygen

--- include/Map.h
+++ include/Map.h
@@ -10,43 +10,46 @@ using namespace std;
 const int MapWidth = 80;
 const int MapHeight = 40;
 
-
+/// A Map container.
 class Map
 {
 public:
+	/// Constructor
 	Map();
+	/// Destructor
 	~Map();
 	
-	//The map
+	///Array of MapObjects which make up the map
 	MapObject* MapCell[MapWidth][MapHeight];
-	
-	//List of Map Objects which keep track of time
+	///List of MapObjects which keep track of time (i.e. need to be ticked)
 	vector<MapObject*> TickMapObjects;	
-	
-	//List of Items on the map which keep track of time
+	///List of Items on the map which keep track of time
 	vector<Item*> TickItems;
-	
-	//List of Items on the map
+	///List of Items on the map
 	vector<Item*> Items;
-
-	//List of Beings on the map
+	///List of Beings on the map
 	vector<Being*> Beings;
 	
 	
-
-	void SetTile(MapObject*, int, int);
+	/// Sets a MapObject
+	void SetTile(MapObject*, int, int);		
+	/// Adds a Being to the Map
 	void AddBeing(Being*, int, int);
-	void AddItem(Item *, int, int);
+	/// Adds an Item to the Map
+	void AddItem(Item *, int, int);			
 
+	/// Removes an Item from the Map
 	void RemoveItem(Item*, bool);
+	/// Removes a Being from the Map
 	void RemoveBeing(Being*, bool);
 
+	/// Checks if an Item is on the specified tile
 	bool IsItemOnTile(int x, int y);
-	bool IsBeingOnTile(int x, int y);
-
-	vector<Item*> GetItemsOnTile(int x, int y);
-
-	void Tick();
+	/// Checks if a Being is on the specified tile
+	bool IsBeingOnTile(int x, int y);		
+	
+	/// Returns a list of Items on a tile
+	vector<Item*> GetItemsOnTile(int x, int y);		
 };
 
 #endif
